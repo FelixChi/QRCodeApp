27229ccd0ce1844ef736107e023059b5b3aff7d0
felixchi <felixchi1@hotmail.com>
2018-11-26 23:49:08 +0800

    feature ResultActivity copy result to clipboard

--- app/src/main/java/com/felixchi/qrcodeapp/ResultActivity.kt
+++ app/src/main/java/com/felixchi/qrcodeapp/ResultActivity.kt
@@ -1,8 +1,14 @@
 package com.felixchi.qrcodeapp
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.TextView
+import android.widget.Toast
 import com.google.zxing.client.android.Intents
 import kotlinx.android.synthetic.main.activity_result.*
 
@@ -17,12 +23,37 @@ class ResultActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayShowHomeEnabled(true)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+//        supportActionBar?.displayOptions
         //Init View
         val format = intent.extras.getString(Intents.Scan.RESULT_FORMAT)
         findViewById<TextView>(R.id.tv_format).text = format
         resultTV = findViewById<TextView>(R.id.textv_result)
         _result = intent.extras.getString(Intents.Scan.RESULT)
         resultTV?.text = _result
+        //
+//        copy2Clipboard(_result)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.activity_result_menu, menu)
+        return true;
+    }
+
+    fun copy2Clipboard(text: String?) {
+        val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        clipboard.primaryClip = ClipData.newPlainText("code", text)
+        Toast.makeText(this, "$text Copied", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean = when(item?.itemId) {
+        R.id.action_copy -> {
+            //
+            copy2Clipboard(_result)
+            true
+        }
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
